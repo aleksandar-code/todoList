25cd4e94d1faee631be76a0b1cf1f923396b2f5e
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-03-12 17:00:55 +0100

    Setup and initialize project

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,29 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    index: "src/index.js",
+    todo: "src/controllers/todo.js",
+    project: "src/controllers/project.js",
+  },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "template-webpack",
+      template: "src/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
