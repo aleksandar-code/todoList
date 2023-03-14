b994bbf7a17253c77e297d2ac920bc836e3c13dd
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-03-14 15:24:00 +0100

    Can create todo and append it to the default project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import "./index.css";
-// import projectModule from "./modules/project";
+import projectModule from "./modules/project";
 
 const formIsComplete = () => {
   const formTitle = document.getElementById("title");
   if (formTitle.value === "") {
-    console.log("empty title");
     return false;
   }
   return true;
 };
+
+const getTodoValues = () => {
+  const title = document.getElementById("title").value;
+  const project = document.getElementById("project-name").textContent;
+  return [title, project];
+};
+
+const addToProject = (e) => {
+  projectModule.createTodo(getTodoValues()[0], getTodoValues()[1]);
+  e.preventDefault();
+};
+
 const submitButton = document.querySelector("button[type=submit]");
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", (e) => {
   if (formIsComplete() === true) {
-    console.log("create todo");
+    addToProject(e);
+    console.log(projectModule.getList());
   }
 });
